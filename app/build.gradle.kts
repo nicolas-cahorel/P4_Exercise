709d1806ad7ef9e47d7dbcc76f18b8cbba914a2c
Nicolas Cahorel <nicolas.cahorel@gmail.com>
2024-10-17 11:15:58 +0200

    Reorganizing repositories and app names

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,11 @@ android {
 }
 
 dependencies {
-    implementation("com.google.ai.client.generativeai:generativeai:0.6.0")
 
+    // Google AI
+    implementation("com.google.ai.client.generativeai:generativeai:0.7.0")
+
+    // Testing Dependencies
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation ("org.mockito:mockito-core:5.11.0")
     testImplementation("junit:junit:4.13.2")
@@ -62,26 +65,29 @@ dependencies {
 
     // Kotlin extensions for core Android libraries
     implementation("androidx.core:core-ktx:1.13.1")
-    // Support library for material design UI components
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    // Material Design components
+
+    // AndroidX Core components
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    // Layout manager for Android
+
+    // RecyclerView and Activity components
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    // RecyclerView library for displaying large sets of data efficiently
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    // ViewModel
-    val viewModelVersion = "2.8.0"
+
+    // ViewModel components
+    val viewModelVersion = "2.8.1"
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$viewModelVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$viewModelVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$viewModelVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$viewModelVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$viewModelVersion")
-    // Koin for Android
+
+    // Koin for Android Dependency Injection
     val koinVersion = "3.5.6"
     implementation("io.insert-koin:koin-android:$koinVersion")
     implementation("io.insert-koin:koin-core:$koinVersion")
-    // Kotlin coroutines
+
+    // Kotlin coroutines components
     val coroutineVersion = "1.7.3"
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "OCRNotes"
+rootProject.name = "P4E_OCRNotes"
 include(":app")
